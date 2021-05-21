9efed1079bfd92b540853cb481cf71b5642505cc
true_mogician <925269713@qq.com>
2021-05-21 20:07:42 +0800

    feat: statement analysis

--- Fetcher/Fetcher.py
+++ Fetcher/Fetcher.py
@@ -1,5 +1,6 @@
 from io import TextIOWrapper
-from typing import Callable, Literal
+from typing import Any, Callable, Literal
+import typing
 from zlib import error as Error
 from pandas.core.frame import DataFrame
 from pathlib import Path
@@ -20,119 +21,135 @@ class HiddenPrints:
         System.stdout = self._original_stdout
 
 
+rawMapper: Callable[[str], str] = lambda x: x
 priceMapper: Callable[[str], int] = lambda x: int(float(x)*10000)
-rateMapper: Callable[[str], int] = lambda x: int(float(x)*1000000)
+rateMapper: Callable[[str], int] = lambda x: None if x == None or x == "" else int(
+    float(x)*1000000)
 intMapper: Callable[[str], int] = lambda x: int(x)
+floorMapper: Callable[[
+    str], int] = lambda x: None if x == None or x == "" else int(x.split(".")[0])
 timeMapper: Callable[[
     str], str] = lambda time: f"{time[0:4]}-{time[4:6]}-{time[6:8]}T{time[8:10]}:{time[10:12]}:{time[12:14]}.{time[14:]}Z"
 rehabilitationMapper: Callable[[
     str], str] = lambda x: "1" if x == "post" else "2" if x == "pre" else "3"
 
-priceKeyMapBase = {
-    "open": "opening",
-    "close": "closing",
-    "high": "highest",
-    "low": "lowest",
-    "volume": "volume",
-    "amount": "turnover",
-    "adjustflag": "rehabilitation"
+priceMapBase: dict[str, tuple[str, Callable[[str], Any]]] = {
+    "open": ("opening", priceMapper),
+    "close": ("closing", priceMapper),
+    "high": ("highest", priceMapper),
+    "low": ("lowest", priceMapper),
+    "volume": ("volume", intMapper),
+    "amount": ("turnover", priceMapper),
+    "adjustflag": ("rehabilitation", rehabilitationMapper)
 }
-priceValueMapBase = {
-    "open": priceMapper,
-    "close": priceMapper,
-    "high": priceMapper,
-    "low": priceMapper,
-    "volume": intMapper,
-    "amount": priceMapper,
-    "adjustflag": rehabilitationMapper
+getStockInfoMap: dict[str, tuple[str, Callable[[str], Any]]] = {
+    "code": ("id", rawMapper),
+    "code_name": ("name", rawMapper),
+    "ipoDate": ("listedDate", rawMapper),
+    "outDate": ("delistedDate", rawMapper),
+    "type": ("type", lambda x: "stock" if x == "1" else "index" if x == "2" else "other"),
+    "industry": ("industry", rawMapper),
+    "industryClassification": ("classification", rawMapper)
 }
-
-
-def getStockInfo(id: str):
-    pass
-
-
-getStockInfoKeyMap = {
-    "code": "id",
-    "code_name": "name",
-    "ipoDate": "listedDate",
-    "outDate": "delistedDate",
-    "type": "type",
-    "industry": "industry",
-    "industryClassification": "classification"
+getStockListMap: dict[str, tuple[str, Callable[[str], Any]]] = {
+    "code": ("id", rawMapper),
+    "code_name": ("name", rawMapper)
 }
-
-getStockInfoValueMap = {
-    "type": lambda x: "stock" if x == "1" else "index" if x == "2" else "other"
+getMinutelyPriceMap: dict[str, tuple[str, Callable[[str], Any]]] = priceMapBase | {
+    "time": ("time", timeMapper)
 }
-
-
-def getStockList(type: str, date: str):
-    pass
-
-
-getStockListKeyMap = {
-    "code": "id",
-    "code_name": "name"
+getDailyPriceMap: dict[str, tuple[str, Callable[[str], Any]]] = priceMapBase | {
+    "date": ("date", rawMapper),
+    "preclose": ("preClosing", priceMapper),
+    "turn": ("turnoverRate", rateMapper),
+    "peTTM": ("per", rateMapper),
+    "pbMRQ": ("pbr", rateMapper),
+    "psTTM": ("psr", rateMapper),
+    "pcfNcfTTM": ("pcfr", rateMapper),
+    "tradestatus": ("stopped", lambda x: x == "0"),
+    "isST": ("specialTreatment", lambda x: x == "1")
+}
+getWeeklyPriceMap: dict[str, tuple[str, Callable[[str], Any]]] = priceMapBase | {
+    "date": ("date", rawMapper),
+    "turn": ("turnoverRate", rateMapper),
 }
 
-
-def getMinutelyPrice(id: str, begin: str, end: str, frequency: str = "60", rehabilitation: Literal["pre", "post", "none"] = "none"):
-    pass
-
-
-getMinutelyPriceKeyMap = priceKeyMapBase | {
-    "time": "time"
+statementMapBase: dict[str, tuple[str, Callable[[str], Any]]] = {
+    "code": ("id", rawMapper),
+    "pubDate": ("publishDate", rawMapper),
+    "statDate": ("statDate", rawMapper),
 }
-getMinutelyPriceValueMap = priceValueMapBase | {
-    "time": timeMapper
+getProfitabilityMap: dict[str, tuple[str, Callable[[str], Any]]] = statementMapBase | {
+    "roeAvg": ("roe", rateMapper),
+    "npMargin": ("npm", rateMapper),
+    "gpMargin": ("gpm", rateMapper),
+    "netProfit": ("np", floorMapper),
+    "epsTTM": ("eps", rateMapper),
+    "MBRevenue": ("mbr", floorMapper),
+    "totalShare": ("ts", floorMapper),
+    "liqaShare": ("cs", floorMapper)
 }
-
-
-def getDailyPrice(id: str, begin: str, end: str, rehabilitation: Literal["pre", "post", "none"] = "none"):
-    pass
-
-
-getDailyPriceKeyMap = priceKeyMapBase | {
-    "date": "date",
-    "preclose": "preClosing",
-    "turn": "turnoverRate",
-    "peTTM": "per",
-    "pbMRQ": "pbr",
-    "psTTM": "psr",
-    "pcfNcfTTM": "pcfr",
-    "tradestatus": "stopped",
-    "isST": "specialTreatment"
+getOperationalCapabilityMap: dict[str, tuple[str, Callable[[str], Any]]] = statementMapBase | {
+    "NRTurnRatio": ("rtr", rateMapper),
+    "NRTurnDays": ("rtd", rateMapper),
+    "INVTurnRatio": ("itr", rateMapper),
+    "INVTurnDays": ("itd", rateMapper),
+    "CATurnRatio": ("catr", rateMapper),
+    "AssetTurnRatio": ("tatr", rateMapper)
 }
-getDailyPriceValueMap = priceValueMapBase | {
-    "preclose": priceMapper,
-    "turn": rateMapper,
-    "peTTM": rateMapper,
-    "pbMRQ": rateMapper,
-    "psTTM": rateMapper,
-    "pcfNcfTTM": rateMapper,
-    "tradestatus": lambda x: x == "0",
-    "isST": lambda x: x == "1"
+getGrowthAbilityMap: dict[str, tuple[str, Callable[[str], Any]]] = statementMapBase | {
+    "YOYEquity": ("nagr", rateMapper),
+    "YOYAsset": ("tagr", rateMapper),
+    "YOYNI": ("npgr", rateMapper),
+    "YOYEPSBasic": ("bepsgr", rateMapper),
+    "YOYPNI": ("npasgr", rateMapper)
 }
-
-
-def getWeeklyPrice(id: str, begin: str, end: str, frequency: str = "week", rehabilitation: Literal["pre", "post", "none"] = "none"):
-    pass
-
-
-getWeeklyPriceKeyMap = priceKeyMapBase | {
-    "date": "date",
-    "turn": "turnoverRate",
+getSolvencyMap: dict[str, tuple[str, Callable[[str], Any]]] = statementMapBase | {
+    "currentRatio": ("cr", rateMapper),
+    "quickRatio": ("qr", rateMapper),
+    "cashRatio": ("car", rateMapper),
+    "YOYLiability": ("tlgr", rateMapper),
+    "liabilityToAsset": ("dar", rateMapper),
+    "assetToEquity": ("em", rateMapper)
 }
-getWeeklyPriceValueMap = priceValueMapBase | {
-    "turn": rateMapper
+getCashFlowMap: dict[str, tuple[str, Callable[[str], Any]]] = statementMapBase | {
+    "CAToAsset": ("catar", rateMapper),
+    "NCAToAsset": ("fatar", rateMapper),
+    "tangibleAssetToAsset": ("tatar", rateMapper),
+    "ebitToInterest": ("ipm", rateMapper),
+    "CFOToOR": ("oncforr", rateMapper),
+    "CFOToNP": ("oncfnpr", rateMapper),
+    "CFOToGr": ("oncfgrr", rateMapper)
+}
+getPerformanceReportMap: dict[str, tuple[str, Callable[[str], Any]]] = {
+    "code": ("id", rawMapper),
+    "performanceExpPubDate": ("publishDate", rawMapper),
+    "performanceExpStatDate": ("statDate", rawMapper),
+    "performanceExpUpdateDate": ("updateDate", rawMapper),
+    "performanceExpressTotalAsset": ("ta", floorMapper),
+    "performanceExpressNetAsset": ("na", floorMapper),
+    "performanceExpressEPSChgPct": ("epsgr", rateMapper),
+    "performanceExpressROEWa": ("roew", rateMapper),
+    "performanceExpressEPSDiluted": ("epsd", rateMapper),
+    "performanceExpressGRYOY": ("grgr", rateMapper),
+    "performanceExpressOPYOY": ("opgr", rateMapper)
+}
+getPerformanceForcastMap: dict[str, tuple[str, Callable[[str], Any]]] = {
+    "code": ("id", rawMapper),
+    "profitForcastExpPubDate": ("publishDate", rawMapper),
+    "profitForcastExpStatDate": ("statDate", rawMapper),
+    "profitForcastType": ("type", rawMapper),
+    "profitForcastAbstract": ("abstract", rawMapper),
+    "profitForcastChgPctUp": ("npasgrUpperLimit", rateMapper),
+    "profitForcastChgPctDwn": ("npasgrLowerLimit", rateMapper)
 }
 
 
 def getStockInfo(id: str):
     data1 = BaoStock.query_stock_basic(id).get_data()
     data2 = BaoStock.query_stock_industry(id).get_data()
-    return {getStockInfoKeyMap[key]: getStockInfoValueMap[key](data1.at[0, key]) if key in getStockInfoValueMap else data1.at[0, key] for key in data1.keys() if key in getStockInfoKeyMap} | {getStockInfoKeyMap[key]: data2.at[0, key] for key in data2.keys() if key in getStockInfoKeyMap}
+    return {getStockInfoMap[key][0]: getStockInfoMap[key][1](data1.at[0, key]) for key in data1.keys() if key in getStockInfoMap} \
+        | {getStockInfoMap[key][0]: getStockInfoMap[key][1](data2.at[0, key]) for key in data2.keys() if key in getStockInfoMap}
 
 
 def getStockList(type: str = "default", date: str = ""):
@@ -158,51 +175,94 @@ def getStockList(type: str = "default", date: str = ""):
         elif type == "stock":
             data = [x for x in data if x["type"] == "1"]
         for row in data:
-            result.append({getStockListKeyMap[key]: row[key]
-                          for key in keys if key in getStockListKeyMap})
+            result.append({getStockListMap[key][0]: row[key]
+                          for key in keys if key in getStockListMap})
         return result
     for row in data.iterrows():
-        result.append({getStockListKeyMap[key]: row[1][key]
-                      for key in data.keys() if key in getStockListKeyMap})
+        result.append({getStockListMap[key][0]: row[1][key]
+                      for key in data.keys() if key in getStockListMap})
     return result
 
 
-def getMinutelyPrice(id: str, begin: str, end: str, frequency: str = "60", rehabilitation: Literal["pre", "post", "none"] = "none"):
+def getMinutelyPrice(id: str, begin: str = None, end: str = None, frequency: str = "60", rehabilitation: Literal["pre", "post", "none"] = "none"):
     result = []
     rehabilitation = "1" if rehabilitation == "post" else "2" if rehabilitation == "pre" else "3"
     data = BaoStock.query_history_k_data_plus(
-        id, ",".join(getMinutelyPriceKeyMap.keys()), begin, end, frequency, rehabilitation).get_data()
+        id, ",".join(getMinutelyPriceMap.keys()), begin, end, frequency, rehabilitation).get_data()
     for row in data.iterrows():
-        dataRow = {getMinutelyPriceKeyMap[key]: getMinutelyPriceValueMap[key](row[1][key]) if key in getMinutelyPriceValueMap else row[1][key]
-                   for key in data.keys() if key in getMinutelyPriceKeyMap}
+        dataRow = {getMinutelyPriceMap[key][0]: getMinutelyPriceMap[key][1](
+            row[1][key]) for key in data.keys() if key in getMinutelyPriceMap}
         dataRow["id"] = id
         result.append(dataRow)
     return result
 
 
-def getDailyPrice(id: str, begin: str, end: str, rehabilitation: Literal["pre", "post", "none"] = "none"):
+def getDailyPrice(id: str, begin: str = None, end: str = None, rehabilitation: Literal["pre", "post", "none"] = "none"):
     result = []
     rehabilitation = "1" if rehabilitation == "post" else "2" if rehabilitation == "pre" else "3"
     data = BaoStock.query_history_k_data_plus(
-        id, ",".join(getDailyPriceKeyMap.keys()), begin, end, "d", rehabilitation).get_data()
+        id, ",".join(getDailyPriceMap.keys()), begin, end, "d", rehabilitation).get_data()
     for row in data.iterrows():
-        dataRow = {getDailyPriceKeyMap[key]: getDailyPriceValueMap[key](row[1][key]) if key in getDailyPriceValueMap else row[1][key]
-                   for key in data.keys() if key in getDailyPriceKeyMap}
+        dataRow = {getDailyPriceMap[key][0]: getDailyPriceMap[key][1](
+            row[1][key]) for key in data.keys() if key in getDailyPriceMap}
         dataRow["id"] = id
         result.append(dataRow)
     return result
 
 
-def getWeeklyPrice(id: str, begin: str, end: str, frequency: str = "week", rehabilitation: Literal["pre", "post", "none"] = "none"):
+def getWeeklyPrice(id: str, begin: str = None, end: str = None, frequency: str = "week", rehabilitation: Literal["pre", "post", "none"] = "none"):
     result = []
     rehabilitation = "1" if rehabilitation == "post" else "2" if rehabilitation == "pre" else "3"
     data = BaoStock.query_history_k_data_plus(
-        id, ",".join(getWeeklyPriceKeyMap.keys()), begin, end, "w" if frequency == "week" else "m", rehabilitation).get_data()
+        id, ",".join(getWeeklyPriceMap.keys()), begin, end, "w" if frequency == "week" else "m", rehabilitation).get_data()
     for row in data.iterrows():
-        dataRow = {getWeeklyPriceKeyMap[key]: getWeeklyPriceValueMap[key](row[1][key]) if key in getWeeklyPriceValueMap else row[1][key]
-                   for key in data.keys() if key in getWeeklyPriceKeyMap}
+        dataRow = {getWeeklyPriceMap[key][0]: getWeeklyPriceMap[key][1](
+            row[1][key]) for key in data.keys() if key in getWeeklyPriceMap}
         dataRow["id"] = id
         result.append(dataRow)
+    return result
+
+
+def getProfitability(id: str, yea: str = None, quarter: str = None):
+    data = BaoStock.query_profit_data(id, yea, quarter).get_data()
+    return {getProfitabilityMap[key][0]: getProfitabilityMap[key][1](data.at[0, key]) for key in data.keys() if key in getProfitabilityMap}
+
+
+def getOperationalCapability(id: str, yea: str = None, quarter: str = None):
+    data = BaoStock.query_operation_data(id, yea, quarter).get_data()
+    return {getOperationalCapabilityMap[key][0]: getOperationalCapabilityMap[key][1](data.at[0, key]) for key in data.keys() if key in getOperationalCapabilityMap}
+
+
+def getGrowthAbility(id: str, yea: str = None, quarter: str = None):
+    data = BaoStock.query_growth_data(id, yea, quarter).get_data()
+    return {getGrowthAbilityMap[key][0]: getGrowthAbilityMap[key][1](data.at[0, key]) for key in data.keys() if key in getGrowthAbilityMap}
+
+
+def getSolvency(id: str, yea: str = None, quarter: str = None):
+    data = BaoStock.query_balance_data(id, yea, quarter).get_data()
+    return {getSolvencyMap[key][0]: getSolvencyMap[key][1](data.at[0, key]) for key in data.keys() if key in getSolvencyMap}
+
+
+def getCashFlow(id: str, yea: str = None, quarter: str = None):
+    data = BaoStock.query_cash_flow_data(id, yea, quarter).get_data()
+    return {getCashFlowMap[key][0]: getCashFlowMap[key][1](data.at[0, key]) for key in data.keys() if key in getCashFlowMap}
+
+
+def getPerformanceReport(id: str, begin: str = None, end: str = None):
+    data = BaoStock.query_performance_express_report(id, begin, end).get_data()
+    result = []
+    for row in data.iterrows():
+        result.append({getPerformanceReportMap[key][0]: getPerformanceReportMap[key][1](
+            row[1][key]) for key in data.keys() if key in getPerformanceReportMap})
+    return result
+
+
+def getPerformanceForecast(id: str, begin: str = None, end: str = None):
+    data = BaoStock.query_forecast_report(id, begin, end).get_data()
+    result = []
+    for row in data.iterrows():
+        result.append({getPerformanceForcastMap[key][0]: getPerformanceForcastMap[key][1](
+            row[1][key]) for key in data.keys() if key in getPerformanceForcastMap})
     return result
 
 
@@ -210,10 +270,10 @@ startTime = Time(9, 30)
 endTime = Time(15)
 
 
-def checkTradeStatus(date: str = ""):
-    if date == "" and (DateTime.now().time() < startTime or DateTime.now().time() > endTime):
+def checkTradeStatus(date: str = None):
+    if date == None and (DateTime.now().time() < startTime or DateTime.now().time() > endTime):
         return "false"
-    if date=="":
+    if date == None:
         date = str(DateTime.now().date())
     return "true" if BaoStock.query_trade_dates(date, date).get_row_data()[1] == "1" else "false"
 
@@ -242,6 +302,20 @@ if __name__ == "__main__":
             result = getWeeklyPrice(*args)
         elif operation == "checkTradeStatus":
             result = checkTradeStatus(*args)
+        elif operation == "getProfitability":
+            result = getProfitability(*args)
+        elif operation == "getOperationalCapability":
+            result = getOperationalCapability(*args)
+        elif operation == "getGrowthAbility":
+            result = getGrowthAbility(*args)
+        elif operation == "getSolvency":
+            result = getSolvency(*args)
+        elif operation == "getCashFlow":
+            result = getCashFlow(*args)
+        elif operation == "getPerformanceReport":
+            result = getPerformanceReport(*args)
+        elif operation == "getPerformanceForcast":
+            result = getPerformanceForecast(*args)
         elif operation == "exit":
             with HiddenPrints():
                 BaoStock.logout()
