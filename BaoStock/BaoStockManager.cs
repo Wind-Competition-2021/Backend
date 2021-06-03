570d5ca573f8304c04f673efdb0acf04475278fa
true_mogician <925269713@qq.com>
2021-06-03 10:33:35 +0800

    style: cleanup

--- BaoStock/BaoStockManager.cs
+++ BaoStock/BaoStockManager.cs
@@ -9,46 +9,43 @@ using Newtonsoft.Json;
 namespace BaoStock {
 	public class BaoStockManager {
 		/// <summary>
-		/// Initialize controller with injected objects
+		///     Initialize controller with injected objects
 		/// </summary>
 		/// <param name="settings">Injected serializer settings</param>
 		public BaoStockManager(JsonSerializerSettings settings) {
-			string fetcherRoot = Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent!.FullName, "BaoStock");
-			string venvPath = Path.Combine(fetcherRoot, "Venv");
-			string pythonPath = Directory.GetDirectories(venvPath)
-				.Select(path => Directory.GetFiles(path, "python*"))
-				.Aggregate(null as string, (path, next) => next.Length > 0 ? next[0] : path);
-			var processInfo = new ProcessStartInfo {
-				FileName = $"\"{pythonPath ?? throw new FileNotFoundException("Python not found in Venv")}\"",
-				Arguments = $"\"{Path.Combine(fetcherRoot, "BaoStock.py")}\"",
-				UseShellExecute = false,
-				RedirectStandardInput = true,
-				RedirectStandardOutput = true,
-				StandardOutputEncoding = Encoding.UTF8,
-				RedirectStandardError = true
+			string curRoot = Directory.GetCurrentDirectory();
+			Process = new Process() {
+				StartInfo = new ProcessStartInfo {
+					FileName = $"py",
+					Arguments = $"-3.9 \"{Path.Combine(curRoot, "BaoStock.py")}\"",
+					UseShellExecute = false,
+					RedirectStandardInput = true,
+					RedirectStandardOutput = true,
+					StandardOutputEncoding = Encoding.UTF8,
+					RedirectStandardError = true
+				}
 			};
-			Process = Process.Start(processInfo);
-			Process!.Exited += (_, _) => {
+			Process.Exited += (_, _) => {
 				Console.Error.WriteLine("BaoStock exited", Color.Red);
 				if (!Process.StandardError.EndOfStream)
 					Console.Error.WriteLine(Process.StandardError.ReadToEnd(), Color.Red);
-				Process = Process.Start(processInfo);
+				Process.Start();
 			};
+			Process.Start();
 			SerializerSettings = settings;
 		}
 
 		/// <summary>
-		/// Python fetcher process
+		///     Python fetcher process
 		/// </summary>
 		public Process Process { get; private set; }
 
 		/// <summary>
-		/// Serializer settings
+		///     Serializer settings
 		/// </summary>
 		public JsonSerializerSettings SerializerSettings { get; }
 
 		/// <summary>
-		/// 
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
 		/// <param name="apiName"></param>
